8c55001e91107ef64b46eb86a548f6f615792539
NPH2019 <54850533+NPH2019@users.noreply.github.com>
2022-01-02 23:44:57 +0700

    update

--- eduApp/settings.py
+++ eduApp/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'edu-app',
         'USER': 'postgres',
-        'PASSWORD': '1973',
+        'PASSWORD': '3003',
         'HOST': 'localhost',
         'PORT': '5432',
         'OPTIONS': {
