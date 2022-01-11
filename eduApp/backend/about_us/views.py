84c4669c03f2341f7171a00cfa7f886c6a1f47bd
tvminh180497 <75160370+tvminh180497@users.noreply.github.com>
2022-01-11 10:34:39 +0700

    Login

---
+++ eduApp/backend/about_us/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect
+from .models import About
+from eduApp.backend.about_us.forms import AboutForm
+
+
+@login_required(login_url='login')
+@require_http_methods(["GET", "POST"])
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index_about.html')
+
+
+@login_required(login_url='login')
+@require_http_methods(["GET", "POST"])
+def create(request):
+    if request.is_ajax and request.method == "POST":
+        obj_about = About()
+        about_data = request.POST.get("textareaValue", None)
+        obj_about.about_description = about_data
+        obj_about.save()
+    return render(request, 'create.html')
+
--- eduApp/frontend/views.py
+++ eduApp/frontend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from eduApp.backend.study_program.models import Program, Class
 from django.views.decorators.http import require_http_methods
+from eduApp.backend.about_us.models import About
 
 
 @require_http_methods(["GET"])
@@ -25,7 +26,10 @@ def detail_class(request, program_id):
 @require_http_methods(["GET"])
 def about(request):
     if request.method == 'GET':
-        return render(request, 'about.html')
+        about_data = About.objects.filter(about_status=True)
+        return render(request, 'about.html', {
+            'about_data': about_data
+        })
 
 
 @require_http_methods(["GET"])
